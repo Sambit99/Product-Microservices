c691876ff7e2c77a5d008c3bee6ae8db045ceda2
Sambit99 <sambitkumar75@gmail.com>
2025-05-24 21:10:43 +0530

    feat: Implement Product model and remove unused service components

--- src/main/java/com/app/microservices/product/model/Product.java
+++ src/main/java/com/app/microservices/product/model/Product.java
@@ -1,5 +1,7 @@
-package com.app.microservices.productservice.model;
+package com.app.microservices.product.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +19,7 @@ import java.math.BigDecimal;
 @Data
 public class Product {
     @Id
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String name;
     private String description;
--- src/main/java/com/app/microservices/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.microservices.productservice.repository;
-
-import com.app.microservices.productservice.model.Product;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, ObjectId> {
-}
